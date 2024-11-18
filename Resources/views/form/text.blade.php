c8b67442dc9bb168bd875bab106ba2019df8ed6d
jzabate <jzabate@melistechnology.com>
2024-11-18 17:47:44 +0800

    jquery migration

--- Resources/views/form/text.blade.php
+++ Resources/views/form/text.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     @if(!is_null($tooltip))
-        {{!  $label .= '<i class="fa fa-info-circle fa-lg tip-info" data-toggle="tooltip" data-placement="left" title="" data-original-title="' . $tooltip . '"></i>' }}
+        {{!  $label .= '<i class="fa fa-info-circle fa-lg tip-info" data-bs-toggle="tooltip" data-bs-placement="left" title="" data-bs-title="' . $tooltip . '"></i>' }}
     @endif
     <label for="{{ $name }}" class="d-flex flex-row justify-content-between">{!! $label !!}</label>
     {{ Form::text($name, $value, array_merge(['class' => 'form-control'], $attributes)) }}
